3f4d4a1bd14b827eb9bcbd01086fc92fcc7b00ba
Bram <brampijper@gmail.com>
2021-05-06 11:33:47 +0200

    created function for dynamically creating pages

---
+++ gatsby-node.js
@@ -0,0 +1,25 @@
+// syntax for importing a module in node
+const path = require('path')
+
+// create pages dynamically and returns a function with a named template
+exports.createPages = async ({graphql, actions}) => {
+    const {createPage} = actions
+    const result = await graphql(`
+    query GetProducts {
+        products: allContentfulServices {
+          nodes {
+            slug
+          }
+        }
+      }
+    `)
+    result.data.products.nodes.forEach((product) => {
+        createPage({
+            path: `/products/${product.slug}`,
+            component:path.resolve(`src/templates/product-template.js`),
+            context: {
+                slug:product.slug
+            }
+        })
+    })
+}
