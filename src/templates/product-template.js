4c6bcda7244c8fa6e626747a64c64d7dacef52bb
Bram <brampijper@gmail.com>
2021-05-06 11:28:49 +0200

    implemented new gatsby image plugin

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import { graphql, Link } from "gatsby"
-// import Image from 'gatsby-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 import Layout from '../components/layout'
-// import styles from '../components/css/single-product.module.css'
-// import Button from '../components/button'
+import Button from '../components/button'
+import * as styles from '../components/css/single-product.module.css'
 
-const productTemplate = ({ data: {product: {title, price, longDescription:{longDescription}}}}) => {
+const productTemplate = ({ data: {product: {title, price, image,  longDescription:{longDescription}}}}) => {
+    const productImage = getImage(image);
     return (
         <Layout>
             <section 
-                // className={styles.page}
+                className={styles.page}
             >
                 <article>
-                    {/* <Image fixed={fixed} alt={title} /> */}
+                    <GatsbyImage image={productImage} alt={`${title} image`} />
                     <Link to="/">back to Home</Link>
                 </article>
                 <article>
                     <h1>{title}</h1>
                     <h3>${price}</h3>
                     <p>{longDescription}</p>
-                    {/* <Button secondary> Get this service </Button> */}
+                    <Button secondary> Get this service </Button>
                 </article>
             </section>
         </Layout>
@@ -34,6 +35,9 @@ export const query = graphql`
           price
           longDescription {
             longDescription
+          }
+          image {
+              gatsbyImageData
           }
         }
       }      
