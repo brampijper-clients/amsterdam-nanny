3760a75f98fcc501f2a215aefc573f34022cb177
Bram <brampijper@gmail.com>
2021-05-07 15:08:53 +0200

    Retrieving and displaying images from an instagram account

--- src/components/socialBlock.js
+++ src/components/socialBlock.js
@@ -1,57 +1,47 @@
 import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
-// import Image from 'gatsby-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import * as styles from './css/social-block.module.css'
 
-// const getData = graphql`
-// query getImages {
-//     allInstagramContent {
-//       nodes {
-//         media_url
-//         id
-//         localImage {
-//           childImageSharp {
-//             fluid(maxHeight: 500, maxWidth: 500, quality: 100) {
-//                 ...GatsbyImageSharpFluid_withWebp
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-
 const getData = graphql`
-query getInstagramTitle {
+query getInstagramContent {
     content: contentfulHomepage {
         title: instagramTitle
+    }
+    instagram: allInstaNode(limit: 12) {
+        nodes {
+          id
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
     }
 }
 `
 
 const ContentBlock = () => {
     const data = useStaticQuery(getData);
-    const { content: {title}} = data
-    // const {
-    //     allInstagramContent: {nodes}
-    // } = data
-    // console.log(nodes)
-
+    const { 
+        content: {title},
+        instagram: {nodes}
+    } = data
     return (
         <section className={styles.page} id="instagram">
             <div className={styles.headlinebg}>
                 <h2>{title}</h2>
             </div>
             <div className={styles.gallery}>
-                {/* {
-                    nodes.map( (image) => {
-                        const { localImage: {childImageSharp: {fluid}} } = image
-                        return <Image key={image.id} fluid={fluid}></Image>
+                {
+                    nodes.map( ({id, localFile} ) => {
+                        const image = getImage(localFile);
+                        return <GatsbyImage image={image} alt={id} key={id} />
                     })
-                } */}
+                }
             </div>
-      </section>
+        </section>
     )
 }
 
