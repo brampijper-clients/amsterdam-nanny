d4b39e026b0bf8b3a56d93e8ed744472e04f2937
Bram <brampijper@gmail.com>
2021-05-07 11:49:54 +0200

    buttontext is now changeable through contentful dashboard

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 import Button from '../components/button'
 import * as styles from '../components/css/single-product.module.css'
 
-const productTemplate = ({ data: {product: {title, price, image,  longDescription:{longDescription}}}}) => {
+const productTemplate = ({ data: {product: {title, price, buttonText, image,  longDescription:{longDescription}}}}) => {
     const productImage = getImage(image);
     return (
         <Layout>
@@ -21,7 +21,7 @@ const productTemplate = ({ data: {product: {title, price, image,  longDescriptio
                     <h1>{title}</h1>
                     <h3>${price}</h3>
                     <p>{longDescription}</p>
-                    <Button secondary> Get this service </Button>
+                    <Button secondary>{buttonText} </Button>
                 </article>
             </section>
         </Layout>
@@ -30,9 +30,10 @@ const productTemplate = ({ data: {product: {title, price, image,  longDescriptio
 
 export const query = graphql`
     query GetSingleProduct($slug:String) {
-        product: contentfulServices(slug: {eq: $slug}) {
+        product: contentfulService(slug: {eq: $slug}) {
           title
           price
+          buttonText
           longDescription {
             longDescription
           }
