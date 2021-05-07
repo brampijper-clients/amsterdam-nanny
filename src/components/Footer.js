0121a3a7ab3579b9f4cd6fd39e210b973a521050
Bram <brampijper@gmail.com>
2021-05-07 11:46:46 +0200

    implement graphql query for getting footer data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import * as styles from './css/footer.module.css'
+import { useStaticQuery, graphql } from "gatsby"
+import MarkdownView from 'react-showdown';
 
+const getData = graphql`
+query GetFooterContent {
+    contentfulHomepage {
+      footer {
+        disclaimer: childContentfulFooterDisclaimerTextNode {
+          text: disclaimer
+        }
+      }
+      logo {
+        gatsbyImageData(
+            placeholder: BLURRED
+            layout: FIXED
+        )
+      }
+    }
+  }
+`
 const Footer = () => {
+    const data = useStaticQuery(getData);
+    const { contentfulHomepage: { footer: { disclaimer: { text }}} } = data 
     return (
         <footer className={styles.page}>
-            <h3> content for footer comes here </h3>
+            <MarkdownView markdown={text}/>
         </footer>
     )
 }
