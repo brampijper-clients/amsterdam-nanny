23e61373f55e8c0177a8bc355f371e70a69c00d8
Bram <brampijper@gmail.com>
2021-05-21 12:11:28 +0200

    navbar logo now has a fixed height

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,7 @@ query getLogo {
             gatsbyImageData(
                 placeholder: BLURRED
                 layout: CONSTRAINED
+                height: 94
             )
         }
     }
