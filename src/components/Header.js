b77e0d01cb678c9ba13214643fa019cbe79b8e4d
Bram <brampijper@gmail.com>
2021-06-15 20:37:24 +0200

    created function for splitting words

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,10 +19,26 @@ const Header = () => {
         contentfulHeader: {headline, headlineButtonText: btnText}
     } = data
 
+    function splitHeadline() {
+        if(headline.startsWith('Flexibel Nanny')) {
+            const headlineArr = headline.split(' ');
+            const firstTwoWords = headlineArr[0].concat(' ', headlineArr[1]);    
+            const restOfHeadline = headlineArr.slice(2).join(' ');
+            return (
+                <h1 className="title">
+                    <span className="bitthai-script">{firstTwoWords}</span>
+                    <span>{restOfHeadline}</span>
+                </h1>
+            )
+        }
+    }
+
+
+
     return (
         <section className={styles.page} id="header">
             <div>
-                <h1>{headline}</h1>
+                {splitHeadline()}
                 <Button email="info@thenannyamsterdam.com">
                     {btnText}
                 </Button>
